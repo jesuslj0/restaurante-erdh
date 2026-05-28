564a35c0bf904830ca9cb332220bd51fa3c4c994
Jesús <jesuslopj0@gmail.com>
2026-05-28 04:24:30 +0200

    Añadiendo scroll ralentizado, probando nuevas fuentes y nuevo bg

---
+++ src/scripts/lenis.js
@@ -0,0 +1,20 @@
+import Lenis from 'lenis';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const lenis = new Lenis({
+  duration: 1.4,
+  smoothWheel: true,
+  wheelMultiplier: 0.65,
+  touchMultiplier: 1,
+});
+
+lenis.on('scroll', ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
